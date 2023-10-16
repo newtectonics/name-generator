b6e3f8c0915fb54aa7ba022328d7bb40c11b30f2
newtectonics <neilzumwalde@gmail.com>
2023-10-16 14:07:31 -0700

    flake.nix

---
+++ flake.nix
@@ -0,0 +1,38 @@
+{
+  description = "A generator to create random names of people";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      # Systems supported
+      allSystems = [
+        "x86_64-linux" # 64-bit Intel/AMD Linux
+        "aarch64-linux" # 64-bit ARM Linux
+        "x86_64-darwin" # 64-bit Intel macOS
+        "aarch64-darwin" # 64-bit ARM macOS
+      ];
+
+      # Helper to provide system-specific attributes
+      forAllSystems = f: nixpkgs.lib.genAttrs allSystems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      packages = forAllSystems ({ pkgs }: {
+        default =
+          let
+            python = pkgs.python39;
+          in
+          python.pkgs.buildPythonApplication {
+            name = "namegen";
+
+            buildInputs = with python.pkgs; [ pip ];
+
+            src = ./.;
+          };
+      });
+    };
+}
