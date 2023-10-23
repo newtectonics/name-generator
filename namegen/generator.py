c8b4f24116df7e21a3b8378e0cd81868bea32455
Neil Zumwalde <neilzumwalde@gmail.com>
2023-10-23 10:46:27 -0700

    Added a name

--- namegen/generator.py
+++ namegen/generator.py
@@ -1,7 +1,7 @@
 import random
 
 def generate_name():
-    first_names = ["Alice", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Hannah", "Ivy", "Jack"]
+    first_names = ["Alice", "Neil", "Bob", "Charlie", "David", "Emma", "Frank", "Grace", "Hannah", "Ivy", "Jack"]
     last_names = ["Smith", "Johnson", "Brown", "Wilson", "Taylor", "Lee", "Clark", "Moore", "Miller", "Hall"]
     
     first_name = random.choice(first_names)
